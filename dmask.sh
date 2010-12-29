3cb3cae8ba8c363a3b676d36a6729506a316fbed
Eotect Nahn <eotect@gmail.com>
2010-12-30 00:16:39 +0800

    Add script dmask.sh for decoding mask file

---
+++ dmask.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ -n "$*" ] ; then
+	exec base64 -d "$@"
+fi
+
+maskfile=
+if [ -f mask ] ; then
+	maskfile=mask
+elif [ -f workspace/mask ] ; then
+	maskfile=workspace/mask
+elif [ -f ../mask ] ; then
+	maskfile=../mask
+elif [ -f ../workspace/mask ] ; then
+	maskfile=../workspace/mask
+elif [ -f ~/mask ] ; then
+	maskfile=~/mask
+elif [ -f ~/.mask ] ; then
+	maskfile=~/.mask
+elif [ -f /myplace/workspace/mask ] ; then
+	maskfile=/myplace/workspace/mask
+elif [ -f /myplace/mask ] ; then
+	maskfile=/myplace/mask
+fi
+
+if [ -n "$maskfile" ] ; then
+	echo "[$maskfile]"
+	base64 -d "$maskfile"
+else
+	base64 -d "$@"
+fi
