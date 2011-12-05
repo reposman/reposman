c6339cb3f1bee5c5c39ef4f2499bb9ba61d47701
Eotect Nahn <eotect@gmail.com>
2011-12-05 21:37:14 +0800

    Move obsolete scripts.

--- init.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set NEWPATH=%NEWPATH%;%FS_ROOT%\workspace\cmd
-SET NEWPATH=%NEWPATH%;%FS_ROOT%\workspace\perl
-SET NEWPATH=%NEWPATH%;%FS_ROOT%\workspace\perl\bin
-SET PERL5LIB=%FS_ROOT%\workspace\perl
-SET XR_PERL_SOURCE_DIR=%FS_ROOT%\workspace\perl
-SET XR_PERL_MODULE_DIR=%FS_ROOT%\workspace\perl\modules
