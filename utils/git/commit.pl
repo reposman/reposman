136c3698813d9f10ad337ca6499f5bfb7428bc41
Eoetct Nahn <eotect@gmail.com>
2012-02-13 04:03:58 +0800

    Remove utils which moved to a standalone repository.

--- utils/git/commit.pl
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-perl ./generate-log.pl . >README 
-
-git add README
-git commit "$@"
--- utils/git/repos_init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-for i in afun dirtyword dirtybase repos ; do 
-	git --bare init $i/git
-	git init $i
-done
